7954415468053e4d5439490502427cec6721dd74
miko <ptechasa@mail.ccsf.edu>
2019-08-18 03:36:16 -0700

    added firebase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,139 +1,221 @@
-var matchingCounter = 30;
-var userName;
-var level;
-var pairsTwoWin = 6;
-var submitted = false;
-var generateImages = [];
-var imagesCounter = 0;
-var imagesDup = [];
-
-//when the player click on image, it will hide 
-// $(document).on('click', '.hide', function () {
-//     $(this).attr('src', '')
-//     console.log('click')
-// })
-
-//Hiding all images
-$('#imgContainer').hide()
-
-//submitting button
-$('#btnSubmit').on('click', function (e) {
-    e.preventDefault()
-
-    //grab the username
-    userName = $('#userName').val()
-    level = $('#level').val()
-
-    //Show images
-    $('#imgContainer').show()
-
-    //Win game for Easy Level is 6
-    if (level === 'easy') {
-        pairsTwoWin = 6
-
-        //Win game for Medium Level is 8
-    } else if (level === 'medium') {
-        pairsTwoWin = 8
-
-        //Win game for Hard Level is 10
-    } else if (level === 'hard') {
-        pairsTwoWin = 10
-    }
-
-    $('#imgContainer').show()
-
-    //Prevent to submit button
-    if (submitted == false) {
-
-        //Decrease timer will start after click 'submit' button 
-        var timer = setInterval(decreaseTimer, 1 * 1000)
-        submitted = true;
-    }
-
-    pushImages()
-})
-
-
-//setup Firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyCbb2wOPgqTLzBV_kwpeFlnuIta9oN8pdg",
-    authDomain: "matching-game-56ad7.firebaseapp.com",
-    databaseURL: "https://matching-game-56ad7.firebaseio.com",
-    projectId: "matching-game-56ad7",
-    storageBucket: "matching-game-56ad7.appspot.com",
-    messagingSenderId: "235534445416",
-    appId: "1:235534445416:web:f70e82fc94bb6d90"
-};
+    apiKey: "AIzaSyBjHkuiXwX8NF68P9suoG15wwn8T24Z0NQ",
+    authDomain: "matching-fun-game.firebaseapp.com",
+    databaseURL: "https://matching-fun-game.firebaseio.com",
+    projectId: "matching-fun-game",
+    storageBucket: "",
+    messagingSenderId: "793782454862",
+    appId: "1:793782454862:web:4223b1cc2cde498b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+var db = firebase.database()
+var formName;
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var queryURL = 'https://api.giphy.com/v1/gifs/random?api_key=N0CBgF93RZv7210eKzxfG7V8BhLtcqg0';
-
-function pushImages() {
-    $.ajax({
-        url: queryURL,
-        method: 'GET',
-    }).then(function (response) {
-        generateImages.push(response.data.images.downsized_large.url)
-        imagesCounter++
-
-        if (imagesCounter < pairsTwoWin) {
-            pushImages()
-        } else {
-            //
-            imagesDup = generateImages.concat(generateImages)
-            shuffle(imagesDup)
-
-            //recursive function
-            imagesDup.forEach(element => {
-                console.log(element)
-
-                var $imagesContainer = $('<div class ="imgContainer">')
-                var $img = $('<img>')
-                $img.attr('src', element);
-
-                $(.imagesContainer).append($img)
-            })
-        }
+function pushName() {
+    formName = $("#userName").val();
+    score = $("#score").text();
+    db.ref().push({
+        name: formName,
+        score: score,
     })
 }
+// $(document).on("click", "#btnSubmit", function() {
+// pushName()
 
+// })
+
+// function pushScoreToFireBase() {
+//     database.ref().push({
+//         userName: userName,
+//         score: score,
+//         counter: counter,
+//         difficulty: difficulty,
+//         dateAdded: firebase.database.ServerValue.TIMESTAMP
+//     });
+// }
+
+// Timer;
+var counter = 30;
+var $span = $('#timer');
+var alertedScore = false;
+
+var interval;
+
+$span.text(counter);
+
+function decreaseTime() {
+    if ((counter == 0) && (alertedScore == false)) {
+        alert('Your score is ' + score);
+        alertedScore = true;
+    }
+
+    counter--;
+    $span.text(counter);
+
+    if (counter == -1) {
+        $span.text('Time is up');
+        clearInterval(interval);
+        pushName();
+    }
+}
+
+// The function to ask their name;
+var userName;
+
+// About the number of blocks;
+var rows = 3;
+var cols = 4;
+
+$('button').on('click', function() {
+    // Input the user names;
+    userName = $('#userName').val();
+
+    interval = setInterval(decreaseTime, 1 * 1000);
+    // Different Levels;
+    var level = $('#level').val()
+    console.log(level)
+    if (level === 'easy') {
+        rows = 3;
+        cols = 4;
+    } else if (level === 'medium') {
+        rows = 4;
+        cols = 4;
+    } else {
+        rows = 4;
+        cols = 5;
+    }
+
+    var imageUrls = [
+        'https://media0.giphy.com/media/l0MYNJC1oGwKLfqso/200.webp?cid=790b7611d34c1c000e51269097b60ed2641e1d871812a2fc&rid=200.webp',
+        'https://media2.giphy.com/media/UtvkbCvOJCJtC/200w.webp?cid=790b7611361e9dda43fa5d4ceadefc1d6ffba77218c5b525&rid=200w.webp',
+        'https://media2.giphy.com/media/k8kA7AOLFVohq/200.webp?cid=790b7611361e9dda43fa5d4ceadefc1d6ffba77218c5b525&rid=200.webp',
+        'https://media1.giphy.com/media/cQz5MLlnP5rfa/200.webp?cid=790b7611561f26b842f49393a2fbf4e32a0542969b591acd&rid=200.webp',
+        'https://media3.giphy.com/media/9MraLzmkWiZqM/200w.webp?cid=790b7611561f26b842f49393a2fbf4e32a0542969b591acd&rid=200w.webp',
+        'https://media3.giphy.com/media/3ov9k4e03yTNRWTgYM/200w.webp?cid=790b7611fc7ad1db280bdc6f9bb012ccc148ab3e0adda710&rid=200w.webp',
+        'https://media0.giphy.com/media/RHJjogamfp4Qm4D9pz/giphy.gif?cid=ecf05e47317383e20249b53a927d22a3f3625315e480e5d2&tid=a632eba0c20cffca6be46e41b0b923982d23f91a95cfd90b2181e30262e60d9d&rid=giphy.gif',
+        'https://media2.giphy.com/media/lOmoovGKkQet9N4uJw/giphy.gif?cid=790b761126043efbe3fc870e111cda1fc0f0733abc621772&rid=giphy.gif',
+        'https://media3.giphy.com/media/L2Gu7sMT7kXR13acn7/giphy.gif?cid=790b7611f15b7653c85e8a769ce525c802a31e7380ecdbda&rid=giphy.gif',
+        'https://media2.giphy.com/media/26DONKguJRB1vZsAM/giphy.gif?cid=790b76118f4e0bee8a27468831bae75a099a9f6f78c6649c&rid=giphy.gif'
+    ];
+    // var imageUrls = 'https://api.giphy.com/v1/gifs/random?api_key=N0CBgF93RZv7210eKzxfG7V8BhLtcqg0';
+
+    var imageIdxs = [];
+    for (var idx = 0; idx < rows * cols; idx++) {
+        imageIdxs.push(Math.floor(idx / 2));
+    }
+    imageIdxs = shuffle(imageIdxs);
+
+    // function pushImages() {
+    //     $.ajax({
+    //         url: imageUrls,
+    //         method: 'GET'
+    //     }).then(function(response) {
+    //         imageIdxs.push(response.data.images.downsized_large.url);
+    //     })
+    // };
+
+    var $container = $('.container');
+    for (var row = 0; row < rows; row++) {
+        var $cardRowElem = $('<div class="row"></div>');
+        for (var col = 0; col < cols; col++) {
+            var idx = row * cols + col;
+            var $card = $('<div class="imgContainer"><img class="hide" src="' + imageUrls[imageIdxs[idx]] + '"></div>');
+            $cardRowElem.append($card);
+        }
+        $container.append($cardRowElem);
+    }
+
+    $('.container').removeClass('hide');
+    $('form').addClass('hide');
+
+    $('#userNameGoesHere').text(userName);
+    $('#h2UserNameGoesHere').removeClass('hide');
+
+    event.preventDefault();
+});
+
+// Score Countdown;
+var score = 0;
+$('#score').text(score);
+
+// How many pair to win;
+var pairsToWin = 6;
+
+// Click Function; 
+var firstSrc;
+var secondSrc;
+
+// Shuffling function: enables that no two games have the same card arrangement;
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-
-        //random index from 0 to i
-        let j = Math.floor(Math.random() * (i + 1));
-        //swap elements
-        [array[i], array[j]] = [array[j], array[i]];
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
-}
+    return array;
+};
 
-//Create countdown timer
-function decreaseTimer() {
-    matchingCounter--;
-    $('.timer').text('Time Remaining: ' + matchingCounter + ' seconds');
+var $firstImg;
+var $secondImg;
 
-    if (matchingCounter == 0) {
-        $('.timer').text('Game over!!')
-        clearInterval(timer)
-    }
-}
+$(document).on('click', '.imgContainer', function() {
 
+    // If either one is empty;
+    if ((firstSrc == undefined) || (secondSrc == undefined)) {
 
+        clearInterval(timer);
 
+        var $img = $(this).children().eq(0);
 
+        // Disable clicking on the last image you clicked on
+        $img.addClass('noClickAllowed');
 
+        var src = $img.attr('src');
 
-// var queryURL = ""
+        // After the user clicks on two divs, check if those images are equal;
+        if (firstSrc == undefined) {
+            firstSrc = src;
+            $firstImg = $img;
+        } else if (secondSrc == undefined) {
+            secondSrc = src;
+            $secondImg = $img;
+        }
+        // If the last two images clicked on match, it would keep them turned over; else, keep them hidden; 
+        var timer = setTimeout(function() {
 
+            // Both are not empty;
+            if ((firstSrc != undefined) && (secondSrc != undefined)) {
+                if (firstSrc != secondSrc) {
+                    $firstImg.addClass('hide');
+                    $secondImg.addClass('hide');
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function(response){
+                    // Disable clicking on the last image you clicked on
+                    $firstImg.parent().removeClass('noClickAllowed');
+                    $secondImg.parent().removeClass('noClickAllowed');
+                } else {
+                    score++;
+                    $('#score').text(score);
 
-//     var img = $('<img>')
-//     img.attr('src', response.)
-//     $('div').prepend(img)
-// });
+                    // Position to win the game;
+                    if ((score == pairsToWin) && (alertedScore == false)) {
+
+                        alertedScore = true;
+                        pushName();
+                        // Let the timer stops;
+                        clearInterval(interval);
+                    }
+                }
+                // Refresh;
+                firstSrc = undefined;
+                secondSrc = undefined;
+            }
+            // The Speed when the cards turn;
+        }, 750);
+    };
+    $img.removeClass('hide');
+});
